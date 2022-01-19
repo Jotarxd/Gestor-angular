c003ad866d8988ba607b409d0e689364bd646fc1
Jotarxd <jotarxd@gmail.com>
2022-01-19 16:52:03 +0100

    cambios login / cambios estilos en master intranet / pagina agenda se agrega div con pacientes

---
+++ Gestor_Clinico_Colegio/Paginas/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Gestor_Clinico_Colegio
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            Response.Redirect("~../Buscador.aspx");
+        }
+    }
+}
